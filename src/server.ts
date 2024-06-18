1a02dba5feea0f686f80c13e689dd42dc1f0bea8
Surith <surithcodes204@gmail.com>
2024-06-19 02:02:10 +0530

    few changes

--- src/server.ts
+++ src/server.ts
@@ -28,10 +28,12 @@ catch(error){
     console.error('Error reading file:', error);
 }
 
+//to check if the port works.
 app.get('/ping' , (req : Request, res : Response) => {
     res.json(true);
 });
 
+//posting the data in the db.
 app.post('/submit' , (req : Request, res : Response) => {
     const {name , email , phone , github_link , stopwatch_time} = req.body;
     const newSubmission : Submission = { name , email , phone , github_link , stopwatch_time};
@@ -51,6 +53,7 @@ app.post('/submit' , (req : Request, res : Response) => {
     res.json({message : 'Submission successful'});
 });
 
+//to read data according to their index.
 app.get('/read' , (req : Request, res : Response) => {
     const index = parseInt(req.query.index as string);
     if(isNaN(index) || index < 0 || index >= submissions.length){
@@ -74,9 +77,9 @@ app.delete('/submissions/:index' , (req : Request, res : Response) => {
     submissions.splice(index, 1);
 
     //need to update the db so that it can be seen that particular data has been removed.
-    try{
+    try{ 
         fs.writeFileSync('db.json', JSON.stringify(submissions));
-        console.log('DB has been updated');
+        console.log('DB has been updated and the submission has been deleted');
     }
     catch(error){
         console.error('Error updating db:', error);
@@ -85,7 +88,41 @@ app.delete('/submissions/:index' , (req : Request, res : Response) => {
     return res.json({message : 'Submission deleted'});
 });
 
+//to edit the forms.
+app.put('/edit/:index' , (req : Request, res : Response) => {
+    const index = parseInt(req.params.index);
+    if(isNaN(index) || index < 0 || index >= submissions.length){
+        res.status(400).json({error : 'Invalid index'});
+    }
+    const {name , email , phone , github_link , stopwatch_time} = req.body;
+    const updatedSubmission : Submission = { name , email , phone , github_link , stopwatch_time};
 
+    submissions[index] = updatedSubmission;
+
+    try{
+        fs.writeFileSync('db.json', JSON.stringify(submissions));
+        console.log('DB has been updated and the submission has been edited');
+    }
+    catch(error){
+        console.error('Error updating db:', error);
+        return res.status(500).json({error : 'Error updating db'});
+    }
+    res.json({message : 'Submission updated successfully'});
+});
+
+
+//to search by email id.
+app.get('/search' , (req : Request, res : Response) => {
+    const email = req.query.email as string;
+    if(!email){
+        return res.status(400).json({error : 'Email is required !'});
+    }
+    const submission = submissions.find(submission => submission.email === email);
+    if(submission){
+        return res.json(submission);
+    }
+    return res.status(404).json({error : 'Submission not found'});
+});
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
